7656a0980905897fb2e1bf4c7e051dd8c48bd4a0
willrob4605 <willrob4605@gmail.com>
2026-06-29 15:47:18 -0500

    Make functions work and now I need to fix them up

--- StreakCounter/StreakCounter.py
+++ StreakCounter/StreakCounter.py
@@ -4,19 +4,35 @@ def addActivity(text,streak):
 def viewAll():
     for key in activity:
         print(f"You have a streak of {activity[key]} days for {key}")
+def viewAllInOrder(arr):
+    for key in arr:
+        print(f"You have a streak of {activity[key]} days for {key}")
 
 #def readFromFile():
 #def addToFile():
-#def sortByHighestStreak():
-    #sorts the keys by highest streak
-    #for loop that goes through all the keys in order
-#def removeActivity
-    #remove data given key
+def sortByHighestStreak():
+    temp=activity
+    highest=0
+    res=[]
+    for i in range(len(activity)):
+        highest=0
+        for key in temp:
+            if temp[key]>highest:
+                highest=temp[key]
+                result=key
+        res.append(result)
+        del temp[key]
+    return res
+def removeActivity(key):
+    del activity[key]
+    
 #def deleteFromFile
-#def resetStreak()
-    #set activity streak value to 0
-#def addToStreak()
-    #add 1 to current streak
+def resetStreak(key):
+    activity[key]+=1
+def addToStreak(key):
+    activity[key]+=1
+
+    
 choice=2
 while choice !=1:
     print("Choose what to do.")
@@ -24,12 +40,16 @@ while choice !=1:
     print("2. Add new activity.")
     print("3. View Highest Streak")
     print("4. Remove Activity")
+    print("Show all activities")
+    choice=int(input())
     if choice==2:
         text=input("Whats the new activity? ")
         streak=input("Whats your current streak ")
         addActivity(text,streak)
     elif choice==3:
-        sortByHighestStreak()
+        arr=sortByHighestStreak()
+        viewAllInOrder(arr)
+
     elif choice==4:
         viewAll()
         number=int(input("Which activty to remove? "))
